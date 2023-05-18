0d1a81a39f00956e8346531d789eaf8eb1dabfaf
unknown <junasnazaritogutib@gmail.com>
2023-05-18 08:55:20 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Stack,Button, AppBar} from "@react-native-material/core";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Junas Nazarito Gutib</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App =() =>(
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  <Stack fill center spacing={4}>
+  <Button title ="Register" onPress={()=> alert("Registration Form")}/>
+
+  <Button title ="Login" onPress={()=> alert("Login Form")}/>
+  </Stack>
+
+  
+);
+
+export default App;
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Junas Nazarito Gutib</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
